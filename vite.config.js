c880d146d1a49710e8231f0290cb4d603de9a9a2
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2023-09-17 22:26:38 +0200

    Upgrade deps and switch to pnpm

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { setDefaultResultOrder } from 'dns'
+
+setDefaultResultOrder('verbatim')
 
 export default defineConfig({
     plugins: [svelte()],
